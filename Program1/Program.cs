2cd08aa06630a498436bcd5a7ba746b76e8aec6a
Roman_Egorov <egorovrp1991@mail.ru>
2023-07-10 23:21:42 +0300

    Program1 stage 1

--- Program1/Program.cs
+++ Program1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число:");
+int n = int.Parse(Console.ReadLine()!);
+int ThreeNumber = 0;
+
+if (n/100 > 0)
+{
+ThreeNumber = (n%1000 - n%100)/100;
+Console.WriteLine(ThreeNumber);
+}
+else
+{
+Console.WriteLine("Третьей цифры нет!");
+}
