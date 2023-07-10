244fd4203268e4fd34d7f33ea7d154c945f15d8c
Roman_Egorov <egorovrp1991@mail.ru>
2023-07-11 00:03:17 +0300

    Program1 and Program -  End

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число:");
+int n = int.Parse(Console.ReadLine()!);
+int SecondNumber = 0;
+
+if(n > 100 && n < 1000)
+{
+SecondNumber = (n / 10) % 10;
+Console.WriteLine("Вторая цифра числа: " + SecondNumber);
+}
+else
+{Console.WriteLine("Число не трёхзначное!");
+}
--- Program1/Program.cs
+++ Program1/Program.cs
@@ -6,11 +6,12 @@
 Console.WriteLine("Введите число:");
 int n = int.Parse(Console.ReadLine()!);
 int ThreeNumber = 0;
-
-if (n/100 > 0)
+if(n>100){
+while (n >= 1000)
 {
-ThreeNumber = (n%1000 - n%100)/100;
-Console.WriteLine(ThreeNumber);
+n = n / 10;
+}
+Console.WriteLine(n % 10);
 }
 else
 {
